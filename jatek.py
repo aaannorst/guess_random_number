f696a8f095c133d0bfc565b3e488af3eb4524c41
Áron, Sánta <aaannorst@gmail.com>
2021-03-09 19:22:52 +0200

    Improved szamjatek

--- jatek.py
+++ jatek.py
@@ -1,4 +1,10 @@
 print("szamjatek")
 nev = input("Mi a neved?\n")
 print("Üdv, {}".format(nev))
+print("Gondoltam egy számra 1 és 100 közt, találd ki!")
+uj_jatek = input("Van-e kedved játszani? [y/n]\n")
+if uj_jatek != "y":
+	print("Viszlát, {}!".format(nev))
+	exit(0)
+print("Játék kezdődik!\n")
 
